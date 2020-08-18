1745c8aacf3d7f2f6bee8062ac09ab2820cd63ca
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-08-18 18:49:36 +0500

    Создаёт первый элемент

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const el = (
+  <div>
+    <input placeholder="search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(el, document.getElementById(`root`));
