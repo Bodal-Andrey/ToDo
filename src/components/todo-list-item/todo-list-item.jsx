b6b185b14284db5163f28a7c35df6b22eaa1aa85
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-08-20 16:43:06 +0500

    Добавить реализацию important

--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -7,9 +7,11 @@ class TodoListItem extends React.Component {
 
     this.state = {
       done: false,
+      important: false,
     };
 
     this.onLabelClick = this.onLabelClick.bind(this);
+    this.onMarkImportant = this.onMarkImportant.bind(this);
   }
 
   onLabelClick() {
@@ -18,24 +20,27 @@ class TodoListItem extends React.Component {
     });
   } 
 
+  onMarkImportant() {
+    this.setState({
+      important: true,
+    });
+  }
+
   render() {
-    const {label, important = false} = this.props;
-    const {done} = this.state;
+    const {label} = this.props;
+    const {done, important} = this.state;
 
     let classNames = `todo-list-item`;
     if (done) {
       classNames += ` done`;
     }
-    
-    const style = {
-    color: important ? `steelblue` : `black`,
-    fontWeight: important ? `bold` : `normal`
-  };
-
+    if (important) {
+      classNames += ` important`
+    }
   return (
     <span className={classNames}>
-      <span onClick={this.onLabelClick} className="todo-list-item-label" style={style}>{label}</span> 
-      <button type="button" className="btn btn-outline-success btn-sm float-right">
+      <span onClick={this.onLabelClick} className="todo-list-item-label">{label}</span> 
+      <button onClick={this.onMarkImportant} type="button" className="btn btn-outline-success btn-sm float-right">
         <i className="fa fa-exclamation" />
       </button>
       <button type="button" className="btn btn-outline-danger btn-sm float-right">
