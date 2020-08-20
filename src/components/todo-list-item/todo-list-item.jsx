b3111226263de28d652d87f60155c5925ecef403
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-08-20 16:02:15 +0500

    Добавит интерактивности

--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import "./todo-list-item.css";
 
 class TodoListItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      done: false,
+    };
+
+    this.onLabelClick = this.onLabelClick.bind(this);
+  }
+
+  onLabelClick() {
+    this.setState({
+      done: true,
+    });
+  } 
+
   render() {
     const {label, important = false} = this.props;
+    const {done} = this.state;
+
+    let classNames = `todo-list-item`;
+    if (done) {
+      classNames += ` done`;
+    }
     
     const style = {
     color: important ? `steelblue` : `black`,
@@ -11,8 +33,8 @@ class TodoListItem extends React.Component {
   };
 
   return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>{label}</span> 
+    <span className={classNames}>
+      <span onClick={this.onLabelClick} className="todo-list-item-label" style={style}>{label}</span> 
       <button type="button" className="btn btn-outline-success btn-sm float-right">
         <i className="fa fa-exclamation" />
       </button>
